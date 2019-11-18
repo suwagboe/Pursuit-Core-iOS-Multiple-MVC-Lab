7f13b8da4f61d66020b74693c0662e1737a511eb
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-18 16:22:56 -0500

    " added view controller to the second UIVIew"

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ZooAnimalDetails.swift
@@ -0,0 +1,21 @@
+//
+//  ZooAnimalDetails.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by World Domination a Brand on 11/18/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+
+import UIKit
+
+class ZooAnimalDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
